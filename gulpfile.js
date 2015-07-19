89fe1b9da33926838a2927f750a781370be55d87
Adam Woods <adam.tomato@gmail.com>
2015-07-20 00:01:33 +0800

    rough layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 
+var templatePath = './views/*.jade';
+
 gulp.task('templates', function() {
-  gulp.src('./views/*.jade')
+  gulp.src(templatePath)
     .pipe(jade())
     .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(templatePath, ['templates']);
 });
 
 gulp.task('default', ['templates']);
