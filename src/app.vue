da45fd4c9b747b88847aebb6b619a46b4ed7e326
Adam Woods <adam.tomato@gmail.com>
2016-02-14 12:04:37 +0800

    using stylus-materialize; added licence numbers

--- src/app.vue
+++ src/app.vue
@@ -3,22 +3,14 @@ div
   header
     .logo-band
       .contained  
-        .logo
-        .call-button(v-el:top-call-button)
-          form
-            button.waves-effect.waves-light(formaction="tel:0894791235")
-              i.icon.icon-phone
-              span 
-              | Call
+        object.logo(type="image/svg+xml", data="{{logo}}")
   main
     .hero
-      .contained
-        {{{hero}}}
+      .contained {{{hero}}}
     .contact-band
       .contained
-        .call-text Get a quote
-        div(v-el:call-button-anchor)
-        .call-button(v-el:call-button)
+        .call-text Contact us
+        .call-button
           form
             button.waves-effect.waves-light(formaction="tel:0894791235")
               i.icon.icon-phone
@@ -33,6 +25,14 @@ div
           h3.work-area-name {{c.name}}
           p.work-area-content {{{c.content}}}
   footer
+    ul.contained
+      li AU38401
+      li ABN 60&nbsp;606&nbsp;101&nbsp;964
+      li EC11874
+      //p Phone - (08) 9479 1235
+    .copyright
+      .contained
+        small © Copyright, Twitch Services · All rights reserved.
 </template>
 
 <script>
@@ -41,6 +41,7 @@ import Waves from 'node-waves'
 export default {
   data () {
     return {
+      logo: require('./logo.svg'),
       hero: require('./markdown/hero.md'),
       categories: [
         {name: 'Residential', content: require('./markdown/residential.md')},
@@ -52,25 +53,6 @@ export default {
 
   ready: function() {
     Waves.init()
-    window.onscroll = this.stickyCall
-    this.stickyCall()
-  },
-
-  methods: {
-    stickyCall: function () {
-      var st = window.pageYOffset
-      var ot = this.$els.callButtonAnchor.getBoundingClientRect().top + st
-      var callButton = this.$els.callButton
-      var topButton = this.$els.topCallButton
-
-      if(st > ot - 10) {
-        callButton.style.display = "none"
-        topButton.style.display = "inline-block"
-      } else if(st <= ot) {
-        callButton.style.display = "inline-block"
-        topButton.style.display = "none"
-      }
-    }
   }
 }
 </script>
@@ -78,92 +60,72 @@ export default {
 <style lang="stylus">
 @import './stylus/master'
 
-band-left = 7/12
-band-right = 5/12
+band-left = 13/24
+band-right = 11/24
 
 $band
-  background-color tamarillo
-  width 100%
-  .contained
-    > :first-child
-      lost-column band-left
-    > :last-child
-      lost-column band-right
+  background-color: $primary-color
+  width: 100%
 
 .logo-band
   @extend $band
-  padding 10px 0 
-  position fixed
-  top 0
-  z-index 100
+  padding: 15px 0
   .logo
-    background-image url("./logo.svg")
-    background-position: 0 50%
-    background-repeat no-repeat
-    background-size 114px 36px
-    height 36px
+    height: 54px
+    width: 100%
 
 .hero
-  padding-top 100px
-  padding-bottom 44px
-  background tallpoppy
-  text-align center
+  text-align: center
+  padding 44px 0
   .headline
-    width 100%
+    width: 100%
     font-size 2.6rem
     line-height 3rem
-    font-family 'Hind'
   .narrative
-    width 100%
-    margin-top 7px
-    font-size 1.2rem
+    width: 100%
+    margin-top: 7px
+    font-size 1.1rem
 
 .contact-band
   @extend $band
-  padding 17px 0 
+  padding: 17px 0 
+  .contained
+    > :first-child
+      lost-column: band-left
+    > :last-child
+      lost-column: band-right
   .call-text
-    lost-column band-left
+    lost-column: band-left
     display: flex;
     font-size: 1.2rem;
-    align-items center
-  .call-button
-    z-index 101
-
+    align-items: center
 
 button
-  width 100%
-  height 36px
-  border: none;
-  border-radius: 2px;
-  outline: 0;
-  text-transform: uppercase;
-  -webkit-tap-highlight-color: transparent;
-
-  text-decoration:none;
-  background-color: tallpoppy;
-  letter-spacing: .5px;
-  transition: .2s ease-out;
-  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-  &:hover
-    background-color: lighten(tallpoppy, 5%)
-    box-shadow: 0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15);
+  sm-button()
+  width: 100%
 
 section
-  color #000
-  margin 50px 0
+  margin: 50px 0
   .work-area
-    lost-row 1/3 50px
-    text-align center
+    lost-row: 1/3 50px
+    text-align: center
     .work-area-icon
-      margin 0 auto
+      margin: 0 auto
     .work-area-name
-      color tamarillo
-      font-size 1.4rem
-      margin 9px 0
+      color: tallpoppy
+      font-size: 1.4rem
+      margin: 9px 0
     .work-area-content
-      font-size 1.1rem
-      ul
-        list-style-type none
+      font-size: 1.1rem
 
+footer
+  sm-footer($copyright-tag: ".copyright", $color: $primary-text-color, $use-container: false)
+  .contained
+    text-align: center
+    padding-bottom: 20px
+  ul
+    width: 100%
+    li
+      lost-column: 1/3
 
 </style>
